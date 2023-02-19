8b1608b34514350342110e2d2244c8ce6a784d47
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-02-19 07:33:46 -0800

    C program that prints the size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    
+    
+    int intType;
+    float floatType;
+    char charType;
+    double doublType;
+    printf("Size of int : %z bytes \n",sizeof(intType));
+    printf("Size of float :%Z bytes \n",sizeof(floatType));
+    printf("Size of Char : %z bytes \n",sizeof(charType));
+    printf("Size of double: %z bytes \n",sizeof(doutType));
+    
+    return 0;
+   
+}
