75a4a6574dd9dd33cd08989ef1fc867dcaff31c6
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-02-19 06:01:15 -0800

    a script that compiles a C file and creates an executable named cisfun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main function for running other functions which will return 0
+ *
+ */
+int main(void)
+{
+	puts("\nProgramming is like building a multilingual puzzle\n");
+	return (0);
+}
