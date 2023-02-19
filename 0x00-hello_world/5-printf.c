1f7850f295823335f68c097f51f520a9c0bbcc4c
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-02-19 06:05:37 -0800

    #include <stdio.h>

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main function for entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("\nWith proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
