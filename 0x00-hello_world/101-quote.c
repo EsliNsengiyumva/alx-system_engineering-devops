17070dcd1c3d78dd748d5fc3467059be501f9eae
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-02-19 07:54:20 -0800

    C program that prints exactly and that piece of art is useful

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main function for entering point
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "Piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+	return (1);
+}
