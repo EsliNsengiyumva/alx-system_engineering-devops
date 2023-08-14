9cbd7c7889d26b5e0d3af7ce41815ae9a6b2c20f
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-08-14 01:34:53 -0700

    updted file

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
-
-def check_for_school(input)
-  regex = /School/
-  if input.match?(regex)
-    puts "Input contains 'School'"
-  else
-    puts "Input does not contain 'School'"
-  end
-end
-
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <input>"
-else
-  input = ARGV[0]
-  check_for_school(input)
-end
-
+puts ARGV[0].scan(/School/).join
